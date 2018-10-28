fcdcec059cd8c907d1e6bc7e3375385c221a4864
Joachim Feltkamp <j.feltkamp@gmail.com>
2018-10-28 15:56:45 +0100

    Continued matrial backend theme

--- themes/argue_base/js/app.js
+++ themes/argue_base/js/app.js
@@ -1,14 +1,64 @@
-
-
 import { MDCTextField } from '@material/textfield';
-import { MDCSelect } from '@material/select';
-import { MDCRipple } from '@material/ripple';
+import { MDCSelect }    from '@material/select';
+import { MDCRipple }    from '@material/ripple';
 import { MDCFormField } from '@material/form-field';
-import { MDCCheckbox } from '@material/checkbox';
+import { MDCCheckbox }  from '@material/checkbox';
+import { MDCList, MDCListFoundation }      from "@material/list";
+import { MDCDrawer }    from "@material/drawer";
+import { MDCTopAppBar } from '@material/top-app-bar/index';
+import { MDCMenu }      from '@material/menu';
+import { MDCMenuSurface } from '@material/menu-surface';
 
 (($, { behaviors }) => {
   behaviors.material = {
     attach(context) {
+      let drawer = {};
+
+      for (let drawerFor of $('.mdc-drawer', context)) {
+        drawer = MDCDrawer.attachTo(drawerFor);
+      }
+
+      for (let list of $('.mdc-list', context)) {
+        let mdclist = MDCList.attachTo(list);
+        mdclist.wrapFocus = true;
+
+        for(let listItem of mdclist.listElements) {
+          let href = listItem.getAttribute("href");
+          if (href) {
+            listItem.addEventListener('click', () => {
+              window.location.pathname = href;
+            });
+          }
+        }
+      }
+
+      for (let topAppBar of $('.mdc-top-app-bar', context)) {
+        let topAppBarInit = MDCTopAppBar.attachTo(topAppBar);
+        topAppBarInit.setScrollTarget(document.getElementById('main-content'));
+        topAppBarInit.listen('MDCTopAppBar:nav', () => {
+          drawer.open = !drawer.open;
+        });
+      }
+
+      for (let menuSurface of $('.mdc-menu-surface', context)) {
+        new MDCMenuSurface(menuSurface);
+      }
+      
+      for (let menu of $('.mdc-menu', context)) {
+        let mdcmenu = new MDCMenu(menu);
+        let toggle = menu.parentElement.querySelector('.js--toggle-menu');
+        if (toggle) {
+          toggle.addEventListener('click', () => {
+            mdcmenu.open = !mdcmenu.open;
+          });
+        } else {
+          mdcmenu.open = true;
+        }
+      }
+
+      for (let ripple of $('.mdc-ripple', context)) {
+        new MDCRipple(ripple);
+      }
 
       for (let textField of $('.mdc-text-field', context)) {
         new MDCTextField(textField);
@@ -29,6 +79,9 @@ import { MDCCheckbox } from '@material/checkbox';
       for (let formField of $('.mdc-form-field', context)) {
         new MDCFormField(formField);
       }
+
+
+
     }
   };
 

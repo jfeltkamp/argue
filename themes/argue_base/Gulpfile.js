3e47661c71882c5855e80c03319625b4b043c780
Joachim Feltkamp <j.feltkamp@gmail.com>
2018-12-09 19:05:24 +0100

    Theming Rules page

---
+++ themes/argue_base/Gulpfile.js
@@ -0,0 +1,68 @@
+var gulp            = require('gulp'),
+  sequence        = require('gulp-sequence'),
+  concat          = require('gulp-concat'),
+  sass            = require('gulp-sass'),
+  sourcemaps      = require('gulp-sourcemaps'),
+  prefixer        = require('gulp-autoprefixer'),
+  readlineSync    = require('readline-sync'),
+  browserSync     = require('browser-sync').create();
+
+
+
+gulp.task('sass', function ()
+{
+  if(process.argv[3] === '--' + 'dev' || process.argv[2] === 'watch')
+  {
+    return gulp
+      .src('./sass/app.scss')
+      .pipe(sourcemaps.init())
+      .pipe(sass({
+        includePaths: ['./node_modules/']
+      }))
+      .pipe(prefixer({
+        browsers: ['last 2 versions', 'IE 11'],
+        cascade: false
+      }))
+      .pipe(sourcemaps.write())
+      .pipe(concat('argue_theme.css'))
+      .pipe(gulp.dest('./assets'))
+      .pipe(browserSync.stream());
+  }
+  else
+  {
+    return gulp
+      .src('./sass/app.scss')
+      .pipe(sass({
+        includePaths: ['./node_modules/'],
+        outputStyle: 'compressed'
+      }))
+      .pipe(prefixer({
+        browsers: ['last 2 versions', 'IE 11'],
+        cascade: false
+      }))
+      .pipe(concat('argue_theme.css'))
+      .pipe(gulp.dest('./assets'));
+  }
+});
+
+gulp.task('watch', function ()
+{
+  browserSync.init({
+    proxy: 'arg_inst.docker.amazee.io'
+  });
+
+  gulp.watch('./sass/**/*', ['sass']);
+  // .on('change', browserSync.reload);
+
+  //gulp.watch('./js/**')
+  //  .on('change', browserSync.reload);
+
+  // basic shared tpls
+  gulp.watch('./templates/**')
+    .on('change', browserSync.reload);
+});
+
+
+gulp.task('default', function(callback) {
+  sequence('sass')(callback);
+});

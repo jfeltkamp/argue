c59cb131c7519bec26d8b6f5141a6f3086c2df6e
Joachim Feltkamp <j.feltkamp@gmail.com>
2021-05-12 13:39:56 +0200

    Improved extend behavior for resize

--- themes/argue_gin/js/card.js
+++ themes/argue_gin/js/card.js
@@ -1,36 +1,88 @@
+/**
+ * @file
+ * Drupal's off-canvas library.
+ */
+(function ($, Drupal, debounce) {
 
-(function ($, Drupal) {
-
-
+  /**
+   * Attaches card behavior for extendable content.
+   *
+   * @type {Drupal~behavior}
+   *
+   * @prop {Drupal~behaviorAttach} attach
+   *   Attaches event listeners for card extendable content.
+   */
   Drupal.behaviors.cardContentExtend = {
-
     attach: function (context) {
+      let self = this;
       /**
        * Make details of argument teaser expandable.
        */
       let $cardExtendableCont = $(context).find('.card__content-item__extend');
+      let eventData = { $elements: $cardExtendableCont };
+
+      self.resetDataHeight(eventData, false);
+
       $cardExtendableCont.each(function (i, cec) {
         let $cec = $(cec);
-        let fullHeight = $cec.height();
-        $cec.data('full-height', fullHeight);
-        let limitedHeight = $cec.data('height');
 
         let $btn = $cec.next('.card__content-item__extend__toggle')
           ||  $cec.prev('.card__content-item__extend__toggle');
-
         $btn.addClass('listen-extend');
-        $cec.css('height', limitedHeight).addClass('content-dense');
+
+        $cec.addClass('content-dense');
+        self.setHeight($cec);
 
         $btn.on('click', function () {
           $btn.toggleClass('show-extend');
-          if ($btn.is('.show-extend')) {
-            $cec.css('height', fullHeight).removeClass('content-dense');
-          } else {
-            $cec.css('height', limitedHeight).addClass('content-dense');
-          }
+          $cec.toggleClass('content-dense');
+          self.setHeight($cec);
         });
+      });
+
+      // Listen on window resize and reset sizes of containers.
+      $(window).on('resize', debounce(function() {
+        self.resetDataHeight(eventData, true);
+      }, 100));
+    },
+
+    /**
+     * Set height on DOM element.
+     *
+     * @param {jQuery} $element
+     *   The off-canvas dialog element.
+     */
+    setHeight: function($element) {
+      if ($element.is('.content-dense')) {
+        $element.css('height', $element.data('dense-height'));
+      } else {
+        $element.css('height', $element.data('full-height'));
+      }
+    },
+
+    /**
+     * Adjusts the dialog on resize.
+     *
+     * @param {object} data
+     *   Data attached to the event.
+     * @param {boolean} setHeight
+     *   Process the resize of .
+     */
+    resetDataHeight: function(data, setHeight) {
+      let self = this;
+      let $elements = data.$elements;
+      $elements.each(function(i, element) {
+        let $elm = $(element);
+        $elm.data('dense-height', $elm.data('dense-height') || '10em')
+        let heightAuto = $elm.css('height', 'auto').height();
+        $elm.data('full-height', heightAuto);
+
+        if (setHeight) {
+          self.setHeight($elm);
+        }
       });
     }
 
   };
-})(jQuery, Drupal);
+
+})(jQuery, Drupal, Drupal.debounce);

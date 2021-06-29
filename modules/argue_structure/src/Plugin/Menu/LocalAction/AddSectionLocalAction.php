69e2076ffa094f52400d02677b8df4c66658c193
Joachim Feltkamp <j.feltkamp@gmail.com>
2021-06-29 10:34:29 +0200

    Action links on section trees including default parameter

---
+++ modules/argue_structure/src/Plugin/Menu/LocalAction/AddSectionLocalAction.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Drupal\argue_structure\Plugin\Menu\LocalAction;
+
+use Drupal\Core\Menu\LocalActionDefault;
+use Drupal\Core\Routing\RouteMatchInterface;
+
+/**
+ * Defines a local action plugin with a dynamic title.
+ */
+class AddSectionLocalAction extends LocalActionDefault {
+
+  /**
+   * {@inheritdoc}
+   *
+   * Get contextual term id and set default query parameter on action link.
+   */
+  public function getOptions(RouteMatchInterface $route_match) {
+    /** @var \Drupal\taxonomy\TermInterface|NULL $term */
+    if ($term = $route_match->getParameters()->get('taxonomy_term')) {
+      return [
+        'query' => [
+          'sector' => $term->id(),
+        ]
+      ];
+    } else return [];
+  }
+
+}
